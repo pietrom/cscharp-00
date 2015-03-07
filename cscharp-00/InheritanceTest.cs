4465f620d9f6bb4685c4a5a1743b8e62595d0d28
Pietro Martinelli <amicofragile@gmail.com>
2015-03-07 19:07:01 +0100

    Polymorphism: iterate over a List<Polygon> to sum up areas and perimeters

--- cscharp-00/InheritanceTest.cs
+++ cscharp-00/InheritanceTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace csharp00
@@ -7,6 +8,26 @@ namespace csharp00
     {
         int getArea();
         int getPerimeter();
+    }
+
+    class MockPolygon : Polygon
+    {
+        private int area, perimeter;
+        public MockPolygon(int area, int perimeter)
+        {
+            this.area = area;
+            this.perimeter = perimeter;
+        }
+
+        public int getArea()
+        {
+            return area;
+        }
+
+        public int getPerimeter()
+        {
+            return perimeter;
+        }
     }
 
     class Rectangle : Polygon
@@ -72,6 +93,13 @@ namespace csharp00
             Assert.AreEqual(60, p.getPerimeter());
         }
         [TestMethod]
+        public void MockPolygon()
+        {
+            Polygon p = new MockPolygon(100, 50);
+            Assert.AreEqual(100, p.getArea());
+            Assert.AreEqual(50, p.getPerimeter());
+        }
+        [TestMethod]
         public void SquareIsARectangle()
         {
             Rectangle r = new Square(10);
@@ -86,6 +114,24 @@ namespace csharp00
             Assert.AreEqual("FOO1", i1.foo());
             I2 i2 = new C();
             Assert.AreEqual(2, i2.foo());
+        }
+
+        [TestMethod]
+        public void Polymorphism()
+        {
+            IList<Polygon> list = new List<Polygon>();
+            list.Add(new Rectangle(10, 20));
+            list.Add(new MockPolygon(20, 20));
+            list.Add(new MockPolygon(30, 30));
+            int at = 0;
+            int pt = 0;
+            foreach(Polygon p in list)
+            {
+                at += p.getArea();
+                pt += p.getPerimeter();
+            }
+            Assert.AreEqual(250, at);
+            Assert.AreEqual(110, pt);
         }
     }
 }
